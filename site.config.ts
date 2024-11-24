4e0d70262a1a09091a8d6dd68df06ee5ee23faf7
Andrej Srna <andrejsrna@MacBook-Pro-uzivatela-Andrej.local>
2024-11-24 18:05:36 +0100

    fix

--- site.config.ts
+++ site.config.ts
@@ -3,5 +3,5 @@ type SiteConfig = {
 };
 
 export const siteConfig: SiteConfig = {
-  site_domain: "http://wordpress-ogc0o84w8o0c480cg8wwcks4.188.245.226.152.sslip.io",
+  site_domain: "https://fitdoplnky",
 };
