fbec370bc48b0a721c1d6bd3f442193e9522c760
andrejsrna <64588471+andrejsrna@users.noreply.github.com>
2024-11-24 18:48:28 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
+        protocol: "https",
         hostname: process.env.WORDPRESS_HOSTNAME,
         port: "",
         pathname: "/**",
