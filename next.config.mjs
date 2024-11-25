d322ead14851841232f14aaed13c0c8ca872a265
andrejsrna <64588471+andrejsrna@users.noreply.github.com>
2024-11-25 17:14:22 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "pub-f4ce0204c4174de68e672172e4a34906.r2.dev", // Allow images from R2 bucket
+        port: "",
+        pathname: "/**",
+      },
     ],
   }, 
   };
